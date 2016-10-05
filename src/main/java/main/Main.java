c0038086e9258070257b810fa2d563c7bcf28901
anton <slonikmak@gmail.com>
2016-10-05 14:15:53 +0200

    new3

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -23,6 +23,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+
         if (args.length>0){
             System.out.println("Path to working base: "+args[0]);
             Context.setDBPath(args[0]);
